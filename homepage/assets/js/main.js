ba516522b98848252d7c7c347c0a240c6ffbcfce
Gaurav Sharma <gauravsain.bharat@gmail.com>
2026-03-06 01:19:50 +0530

    v.08

--- homepage/assets/js/main.js
+++ homepage/assets/js/main.js
@@ -327,3 +327,25 @@ draw();
 
 });
 
+/* =========================================================
+ BILLING MODEL ANIMATION
+========================================================= */
+const target = new Date("April 28, 2026 18:00:00").getTime();
+
+setInterval(function(){
+
+const now = new Date().getTime();
+
+const distance = target - now;
+
+const days = Math.floor(distance/(1000*60*60*24));
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+const seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML=days;
+document.getElementById("hours").innerHTML=hours;
+document.getElementById("minutes").innerHTML=minutes;
+document.getElementById("seconds").innerHTML=seconds;
+
+},1000);
